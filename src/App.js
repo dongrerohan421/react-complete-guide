f1208f0ec07faa817bec1cbca093a465e7397090
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-09 15:05:23 -0400

    render Person component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
     render() {
@@ -7,6 +8,9 @@ class App extends Component {
             <div className="App">
                 <h1> Hi, I am React App </h1>
                 <p>This is working too!</p>
+                <Person />
+                <Person />
+                <Person />
             </div>
         );
         // return React.createElement(
